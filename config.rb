245b1be7271906a6636a263e758c2f6549b04c59
Jackson Pires <jackson.pires@gmail.com>
2017-12-11 20:18:45 +0000

    Alterando banner BT

--- config.rb
+++ config.rb
@@ -41,7 +41,7 @@ end
 data.promotions.banners_html.each do |banner|
   if banner.active?
     proxy "/#{banner.html_page}", "/template_banner.html",
-    :locals => { :banner => banner }, :ignore => true
+   :locals => { :banner => banner }, :ignore => true
   end
 end
 
